21895bae29c7ff6967d3c2ea51d607ad3233774f
231220000-Zhou Zi Qiang <1526279450@qq.com>
2024-09-21 15:18:19 +0800

    update

--- exercises/smart_pointers/arc1.rs
+++ exercises/smart_pointers/arc1.rs
@@ -21,7 +21,7 @@
 //
 // Execute `rustlings hint arc1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
@@ -29,13 +29,18 @@ use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+      let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
-    for offset in 0..8 {
-        let child_numbers = // TODO
+   for offset in 0..8 {
+        let shared_numbers_clone = Arc::clone(&shared_numbers); // 克隆 Arc
+        let child_numbers = shared_numbers_clone.iter().enumerate()
+            .filter(|&(i, _)| i % 8 == offset) // 选择符合偏移条件的元素
+            .map(|(_, &n)| n) // 提取值
+            .collect::<Vec<_>>(); // 收集为 Vec
+
         joinhandles.push(thread::spawn(move || {
-            let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
+            let sum: u32 = child_numbers.iter().sum(); // 计算总和
             println!("Sum of offset {} is {}", offset, sum);
         }));
     }
--- exercises/smart_pointers/cow1.rs
+++ exercises/smart_pointers/cow1.rs
@@ -12,7 +12,7 @@
 //
 // Execute `rustlings hint cow1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::borrow::Cow;
 
@@ -47,8 +47,9 @@ mod tests {
         // No clone occurs because `input` doesn't need to be mutated.
         let slice = [0, 1, 2];
         let mut input = Cow::from(&slice[..]);
-        match abs_all(&mut input) {
-            // TODO
+         match abs_all(&mut input) {
+            Cow::Borrowed(_) => Ok(()), 
+            _ => Err("Expected borrowed value"),
         }
     }
 
@@ -59,8 +60,9 @@ mod tests {
         // still owned because it was never borrowed or mutated.
         let slice = vec![0, 1, 2];
         let mut input = Cow::from(slice);
-        match abs_all(&mut input) {
-            // TODO
+       match abs_all(&mut input) {
+            Cow::Owned(_) => Ok(()), 
+            _ => Err("Expected owned value"),
         }
     }
 
@@ -71,8 +73,9 @@ mod tests {
         // before.
         let slice = vec![-1, 0, 1];
         let mut input = Cow::from(slice);
-        match abs_all(&mut input) {
-            // TODO
+         match abs_all(&mut input) {
+            Cow::Owned(_) => Ok(()), 
+            _ => Err("Expected owned value"),
         }
     }
 }
